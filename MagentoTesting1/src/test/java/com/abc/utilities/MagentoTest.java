e7a48974dcaa93e967dd236464f9eca7c1384244
BULBUL001 <31031011+BULBUL001@users.noreply.github.com>
2020-05-01 19:03:59 +0530

    Second Commit

--- MagentoTesting1/src/test/java/com/abc/utilities/MagentoTest.java
+++ MagentoTesting1/src/test/java/com/abc/utilities/MagentoTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import com.abc.magentoobjects.Home;
@@ -13,7 +14,7 @@ import com.abc.magentoobjects.Welcome;
 public class MagentoTest {
 	@Test
 	public void positiveCredential() throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("https://www.magento.com");
